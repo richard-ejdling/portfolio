2a6ea4552b18c28be029149385ee25b0f5ec9efe
Richard Ejdling <richard.ejdling@chasacademy.se>
2023-04-13 10:19:10 +0200

    first commit

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,26 @@
 import '@/styles/globals.css'
+import { GoMarkGithub } from 'react-icons/go'
+import { BsLinkedin } from 'react-icons/bs'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <header className='flex items-center'>
+        <h1>Richard<br />Ejdling</h1>
+        <nav className='flex items-center'>
+          <a href="https://github.com/chasacademy-richard-ejdling"><GoMarkGithub /></a>
+          <a href=""><BsLinkedin /></a>
+          <ul className='flex items-center'>
+            <li>Home</li>
+            <li>Projects</li>
+            <li>Technologies</li>
+            <li>About</li>
+            <li>Contact</li>
+          </ul>
+        </nav>
+      </header>
+
+      <Component {...pageProps} />
+    </div>
+  )
 }
