f9814132372e4deb5004fdc4ce2a7f609d3640dd
Richard Ejdling <richard.ejdling@chasacademy.se>
2023-04-13 12:48:18 +0200

    created header and nav menu

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,24 +1,48 @@
 import '@/styles/globals.css'
 import { GoMarkGithub } from 'react-icons/go'
 import { BsLinkedin } from 'react-icons/bs'
+import { HiMenu } from 'react-icons/hi'
+import { RxCross2 } from 'react-icons/rx'
+import Link from 'next/link'
+import { useState } from 'react'
 
 export default function App({ Component, pageProps }) {
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+
   return (
-    <div>
-      <header className='flex items-center'>
-        <h1>Richard<br />Ejdling</h1>
-        <nav className='flex items-center'>
-          <a href="https://github.com/chasacademy-richard-ejdling"><GoMarkGithub /></a>
-          <a href=""><BsLinkedin /></a>
-          <ul className='flex items-center'>
-            <li>Home</li>
-            <li>Projects</li>
-            <li>Technologies</li>
-            <li>About</li>
-            <li>Contact</li>
+    <div style={{ backgroundImage: 'url(' + 'https://www.alleycat.org/wp-content/uploads/2019/03/FELV-cat.jpg' + ')' }}>
+      <header className='flex items-center sticky top-0 left-0 p-2 backdrop-blur-sm bg-sky-600/75 justify-between'>
+        <h1>RE</h1>
+        <nav className='flex items-center gap-4'>
+          <div className='flex gap-2'>
+            <a href="https://github.com/chasacademy-richard-ejdling"><GoMarkGithub /></a>
+            <a href=""><BsLinkedin /></a>
+          </div>
+          <button className='sm:hidden' onClick={() => setIsMenuOpen(true)}><HiMenu /></button>
+          <ul className='hidden sm:flex items-center gap-2'>
+            <li><Link href='/'>Home</Link></li>
+            <li><Link href='/projects'>Projects</Link></li>
+            <li><Link href='/technologies'>Technologies</Link></li>
+            <li><Link href='/about'>About</Link></li>
+            <li><Link href='/contact'>Contact</Link></li>
           </ul>
         </nav>
       </header>
+
+      {isMenuOpen && (
+        <div id='close' className='flex justify-end backdrop-blur backdrop-brightness-[0.7] fixed top-0 right-0 h-full w-full' onClick={(e) => e.target.id === 'close' && setIsMenuOpen(false)}>
+          <div className='flex flex-col w-40 h-fit sm:hidden z-10 bg-sky-600 m-2 p-2 rounded-lg'>
+            <button className='self-end' onClick={() => setIsMenuOpen(false)}><RxCross2 /></button>
+            <ul className='self-start'>
+              <li><Link href='/'>Home</Link></li>
+              <li><Link href='/projects'>Projects</Link></li>
+              <li><Link href='/technologies'>Technologies</Link></li>
+              <li><Link href='/about'>About</Link></li>
+              <li><Link href='/contact'>Contact</Link></li>
+            </ul>
+          </div>
+        </div>
+      )}
 
       <Component {...pageProps} />
     </div>
