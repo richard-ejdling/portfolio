917ab5bc3ed36209ff93f3bf5aa702e856abec09
Richard Ejdling <richard.ejdling@chasacademy.se>
2023-04-13 14:02:28 +0200

    further fixed nav menu

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,9 +5,50 @@ import { HiMenu } from 'react-icons/hi'
 import { RxCross2 } from 'react-icons/rx'
 import Link from 'next/link'
 import { useState } from 'react'
+import { useRouter } from 'next/router'
 
 export default function App({ Component, pageProps }) {
   const [isMenuOpen, setIsMenuOpen] = useState(false)
+
+  const router = useRouter()
+
+  function NavList({ isModal }) {
+    let key = 0
+    const navList = [
+      {
+        name: 'Home',
+        path: '/'
+      },
+      {
+        name: 'Projects',
+        path: '/projects'
+      },
+      {
+        name: 'Technologies',
+        path: '/technologies'
+      },
+      {
+        name: 'About',
+        path: '/about'
+      },
+      {
+        name: 'Contact',
+        path: '/contact'
+      }
+    ]
+
+    return (
+      <ul className={isModal ? 'self-start' : 'hidden sm:flex items-center gap-2'}>
+        {navList.map(item => {
+          if (item.path === router.pathname) {
+            return <li key={key++}><button onClick={isModal ? () => setIsMenuOpen(false) : () => {}} className='underline'>{item.name}</button></li>
+          } else {
+            return <li key={key++}><Link onClick={() => setIsMenuOpen(false)} href={item.path}>{item.name}</Link></li>
+          }
+        })}
+      </ul>
+    )
+  }
 
   return (
     <div style={{ backgroundImage: 'url(' + 'https://www.alleycat.org/wp-content/uploads/2019/03/FELV-cat.jpg' + ')' }}>
@@ -15,17 +56,18 @@ export default function App({ Component, pageProps }) {
         <h1>RE</h1>
         <nav className='flex items-center gap-4'>
           <div className='flex gap-2'>
-            <a href="https://github.com/chasacademy-richard-ejdling"><GoMarkGithub /></a>
-            <a href=""><BsLinkedin /></a>
+            <a href="https://github.com/chasacademy-richard-ejdling" target='_blank'><GoMarkGithub /></a>
+            <a href="" target='_blank'><BsLinkedin /></a>
           </div>
           <button className='sm:hidden' onClick={() => setIsMenuOpen(true)}><HiMenu /></button>
-          <ul className='hidden sm:flex items-center gap-2'>
+          {/* <ul className='hidden sm:flex items-center gap-2'>
             <li><Link href='/'>Home</Link></li>
             <li><Link href='/projects'>Projects</Link></li>
             <li><Link href='/technologies'>Technologies</Link></li>
             <li><Link href='/about'>About</Link></li>
             <li><Link href='/contact'>Contact</Link></li>
-          </ul>
+          </ul> */}
+          <NavList isModal={false} />
         </nav>
       </header>
 
@@ -33,13 +75,14 @@ export default function App({ Component, pageProps }) {
         <div id='close' className='flex justify-end backdrop-blur backdrop-brightness-[0.7] fixed top-0 right-0 h-full w-full' onClick={(e) => e.target.id === 'close' && setIsMenuOpen(false)}>
           <div className='flex flex-col w-40 h-fit sm:hidden z-10 bg-sky-600 m-2 p-2 rounded-lg'>
             <button className='self-end' onClick={() => setIsMenuOpen(false)}><RxCross2 /></button>
-            <ul className='self-start'>
+            {/* <ul className='self-start'>
               <li><Link href='/'>Home</Link></li>
               <li><Link href='/projects'>Projects</Link></li>
               <li><Link href='/technologies'>Technologies</Link></li>
               <li><Link href='/about'>About</Link></li>
               <li><Link href='/contact'>Contact</Link></li>
-            </ul>
+            </ul> */}
+            <NavList isModal={true} />
           </div>
         </div>
       )}
