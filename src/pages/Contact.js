79b33482a4d590bc09dbb12ab76845acb22fc5f3
Richard Ejdling <richard.ejdling@chasacademy.se>
2024-10-18 12:26:13 +0200

    contact mail copy

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,80 +1,29 @@
+import { useState } from "react";
+import { MdCopyAll } from "react-icons/md";
+import { FaCheck } from "react-icons/fa6";
+
 export default function Contact() {
+  const [copied, setCopied] = useState(false)
+
+  function handleCopy() {
+    navigator.clipboard.writeText("richardej@hotmail.se")
+    setCopied(true)
+
+    setTimeout(()=> setCopied(false), 3000)
+  }
+
   return (
-    <div>
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minima
-      voluptatem quam tempora rem vitae qui quidem debitis magni, voluptas
-      expedita non eum sint corrupti sapiente a. Cumque commodi repellendus
-      aliquam. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minima
-      voluptatem quam tempora rem vitae qui quidem debitis magni, voluptas
-      expedita non eum sint corrupti sapiente a. Cumque commodi repellendus
-      aliquam. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minima
-      voluptatem quam tempora rem vitae qui quidem debitis magni, voluptas
-      expedita non eum sint corrupti sapiente a. Cumque commodi repellendus
-      aliquam. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minima
-      voluptatem quam tempora rem vitae qui quidem debitis magni, voluptas
-      expedita non eum sint corrupti sapiente a. Cumque commodi repellendus
-      aliquam. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minima
-      voluptatem quam tempora rem vitae qui quidem debitis magni, voluptas
-      expedita non eum sint corrupti sapiente a. Cumque commodi repellendus
-      aliquam. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minima
-      voluptatem quam tempora rem vitae qui quidem debitis magni, voluptas
-      expedita non eum sint corrupti sapiente a. Cumque commodi repellendus
-      aliquam. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minima
-      voluptatem quam tempora rem vitae qui quidem debitis magni, voluptas
-      expedita non eum sint corrupti sapiente a. Cumque commodi repellendus
-      aliquam. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minima
-      voluptatem quam tempora rem vitae qui quidem debitis magni, voluptas
-      expedita non eum sint corrupti sapiente a. Cumque commodi repellendus
-      aliquam. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minima
-      voluptatem quam tempora rem vitae qui quidem debitis magni, voluptas
-      expedita non eum sint corrupti sapiente a. Cumque commodi repellendus
-      aliquam. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minima
-      voluptatem quam tempora rem vitae qui quidem debitis magni, voluptas
-      expedita non eum sint corrupti sapiente a. Cumque commodi repellendus
-      aliquam. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minima
-      voluptatem quam tempora rem vitae qui quidem debitis magni, voluptas
-      expedita non eum sint corrupti sapiente a. Cumque commodi repellendus
-      aliquam. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minima
-      voluptatem quam tempora rem vitae qui quidem debitis magni, voluptas
-      expedita non eum sint corrupti sapiente a. Cumque commodi repellendus
-      aliquam. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minima
-      voluptatem quam tempora rem vitae qui quidem debitis magni, voluptas
-      expedita non eum sint corrupti sapiente a. Cumque commodi repellendus
-      aliquam. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minima
-      voluptatem quam tempora rem vitae qui quidem debitis magni, voluptas
-      expedita non eum sint corrupti sapiente a. Cumque commodi repellendus
-      aliquam. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minima
-      voluptatem quam tempora rem vitae qui quidem debitis magni, voluptas
-      expedita non eum sint corrupti sapiente a. Cumque commodi repellendus
-      aliquam. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minima
-      voluptatem quam tempora rem vitae qui quidem debitis magni, voluptas
-      expedita non eum sint corrupti sapiente a. Cumque commodi repellendus
-      aliquam. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minima
-      voluptatem quam tempora rem vitae qui quidem debitis magni, voluptas
-      expedita non eum sint corrupti sapiente a. Cumque commodi repellendus
-      aliquam. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minima
-      voluptatem quam tempora rem vitae qui quidem debitis magni, voluptas
-      expedita non eum sint corrupti sapiente a. Cumque commodi repellendus
-      aliquam. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minima
-      voluptatem quam tempora rem vitae qui quidem debitis magni, voluptas
-      expedita non eum sint corrupti sapiente a. Cumque commodi repellendus
-      aliquam. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minima
-      voluptatem quam tempora rem vitae qui quidem debitis magni, voluptas
-      expedita non eum sint corrupti sapiente a. Cumque commodi repellendus
-      aliquam. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minima
-      voluptatem quam tempora rem vitae qui quidem debitis magni, voluptas
-      expedita non eum sint corrupti sapiente a. Cumque commodi repellendus
-      aliquam. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minima
-      voluptatem quam tempora rem vitae qui quidem debitis magni, voluptas
-      expedita non eum sint corrupti sapiente a. Cumque commodi repellendus
-      aliquam. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minima
-      voluptatem quam tempora rem vitae qui quidem debitis magni, voluptas
-      expedita non eum sint corrupti sapiente a. Cumque commodi repellendus
-      aliquam. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minima
-      voluptatem quam tempora rem vitae qui quidem debitis magni, voluptas
-      expedita non eum sint corrupti sapiente a. Cumque commodi repellendus
-      aliquam.
-      <p className="text-xl sm:text-lg">Contact info.........</p>
+    <div className="min-h-[calc(100vh-220px)] ">
+      <div className=" flex flex-row items-center text-xl sm:text-lg">
+        <p>Contact me at &nbsp;</p>
+        <div className="flex flex-row items-center gap-2 bg-sky-600 pl-1.5 rounded-md">
+          richardej@hotmail.se
+          <button onClick={handleCopy} className="flex flex-row items-center gap-1 text-base border-l-2 border-white rounded-r-md p-1 h-full bg-sky-700 hover:bg-sky-600 active:bg-sky-600">
+          {copied ? <FaCheck size={20} /> : <MdCopyAll size={20} />}
+            {copied ? "Copied" : "Copy"}
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
