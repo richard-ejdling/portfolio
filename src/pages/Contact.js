50c7bdfc1819f02345c7ac9dbf6837c3d581dd75
Richard Ejdling <richard.ejdling@chasacademy.se>
2024-10-23 13:53:34 +0200

    accessability on copy and form

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -8,26 +8,31 @@ export default function Contact() {
   const timeoutRef = useRef();
 
   function handleCopy() {
-    navigator.clipboard.writeText("richardej@hotmail.se");
-    setCopied(true);
-
     if (timeoutRef.current) {
       clearTimeout(timeoutRef.current);
     }
 
-    timeoutRef.current = setTimeout(() => {
-      setCopied(false);
-      timeoutRef.current = null;
-    }, 3000);
+    navigator.clipboard.writeText("richardej@hotmail.se").then(() => {
+      setCopied(true);
+
+      timeoutRef.current = setTimeout(() => {
+        setCopied(false);
+        timeoutRef.current = null;
+      }, 3000);
+    });
   }
 
   return (
     <div className="min-h-[calc(100vh-220px)] flex flex-col items-center gap-4 text-xl sm:text-lg">
       <div className="flex flex-col gap-2 sm:flex-row sm:gap-0 items-center">
         <h3>Contact me at &nbsp;</h3>
-        <div className="flex flex-row items-center gap-2 bg-sky-600 pl-1.5 rounded-md">
+        <div
+          aria-live="polite"
+          className="flex flex-row items-center gap-2 bg-sky-600 pl-1.5 rounded-md"
+        >
           richardej@hotmail.se
           <button
+            aria-label={"Cypy email adress to clipboard"}
             onClick={handleCopy}
             className="flex flex-row items-center gap-1 text-lg sm:text-base border-l-2 border-white rounded-r-md p-1 h-full bg-sky-700 hover:bg-sky-800 active:bg-sky-900 transition-colors duration-100"
           >
